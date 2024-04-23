4085906080890c48161e339eefa73a605024ae08
Amer Mekic <amerm3kic@gmail.com>
2024-04-23 20:58:11 +0200

    Add exception handling, delete and update movie features

--- controllers/infoController.js
+++ controllers/infoController.js
@@ -36,13 +36,22 @@ const addMovie = asyncHandler( async (req, res) => {
         res.status(400);
         throw new Error("Must Enter valid title!");
     }
-    const response = await axios.get(`http://www.omdbapi.com/?t=${title}&y=${year}&apikey=${process.env.API_KEY}`);
-    console.log(response.data);
+    try{
+    var response = await axios.get(`http://www.omdbapi.com/?t=${title}&y=${year}&apikey=${process.env.API_KEY}`);
+    }
+    catch(err){
+        throw new Error("Enter valid title and year. Check if your API key is active and verify internet connection");
+    }
     const TITLE = response.data.Title;
     const YEAR = response.data.Year;
     const RATING = response.data.imdbRating || 0;
     const PLOT = response.data.Plot;
-    const posta = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.API_KEY2}&query=${TITLE}`);
+    try{
+    var posta = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.API_KEY2}&query=${TITLE}`);
+    }
+    catch(error){
+        throw new Error("Error trying to get movie poster. Check your internet connection and try again");
+    }
     const poster = posta.data.results[0].poster_path;
     console.log(posta.data.results[0].poster_path);
     const POSTER = 'http://image.tmdb.org/t/p/w500'+poster;
@@ -58,7 +67,41 @@ const addMovie = asyncHandler( async (req, res) => {
         error: "Movie has already been added, try again.",
       });*/
     }
+    res.status(200);
+});
+
+const updateMovie = asyncHandler(async (req, res) => {
+    const title = req.body.title;
+    try{
+    const plotQuery = await db.query("SELECT plot FROM watched_movies WHERE title=$1", [title]);
+    const plot = plotQuery.rows;
+    }
+    catch(error){
+        throw new Error({message: "An error occurred while trying to fetch resource from database, try again later"});
+    }
+    const input = "Ed Edd and Eddy";
+    //const input = prompt("Write new plot:", plot);
+    try{
+    const updateDB = await db.query("UPDATE watched_movies SET plot=$1 WHERE title=$2", [input||plot, title]);
+    }
+    catch(error){
+        throw new Error({message: "Error while updating movie"});
+    }
+    res.status(200);
+});
+
+const deleteMovie = asyncHandler(async (req, res) => {
+    const title = req.body.title;
+    try{
+        await db.query("DELETE FROM watched_movies WHERE title=$1", [title]);
+    }
+    catch(error){
+        throw new Error({message: "Error occurred in deleting record, try again later!"});
+    }
+    res.status(200);
 });
 
 export {getMovies};
 export {addMovie};
+export {updateMovie};
+export {deleteMovie};
--- routes/info.js
+++ routes/info.js
@@ -1,8 +1,17 @@
 import express from "express";
-import {getMovies, addMovie} from "../controllers/infoController.js";
+import {getMovies, addMovie, updateMovie, deleteMovie} from "../controllers/infoController.js";
 
 const router = express.Router();
 
-router.route("/").get(getMovies).post(addMovie);
+router.route("/").get(getMovies)
+router.route("/add").post(addMovie,(req,res)=>{
+    res.redirect("/");
+})
+router.route("/update/:id").post(updateMovie,(req,res)=>{
+    res.redirect("/");
+});
+router.route("/delete/:id").post(deleteMovie, (req,res)=>{
+    res.redirect("/");
+});
 
 export default router;
